edfc84a6efc677d876a44bb038ff21ce12056ee6
alexandreCastroOliveira <140731517+alexandreCastroOliveira@users.noreply.github.com>
2025-06-17 21:04:42 +0000

    Nível Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,67 +1,61 @@
 #include <stdio.h>
 
-int main() {
-    /* TORRE
-
-    A torre se move em linha reta, tanto na vertical, quanto na horizontal. Neste exemplo, a torre andará 
-    5 casas para a direita.
-
-    Vou utilizar o loop FOR para esse movimento.*/
-
-    printf("Movimento da Torre:\n");
-    int casasTorre = 5;
-    for (int torre = 0; torre < casasTorre; torre++) {
+// Criando a função recursiiva da Torre
+void moverTorre(int casas) { //Variável casas indica a quantidade de vezes que a função será chamada.
+    if (casas > 0) {
         printf("Direita\n");
+        moverTorre(casas - 1); //Recursividade
     }
+}
 
-    /*BISPO
-
-    O bispo se move na diagonal. Neste exemplo, o bispo andará para cima e para a direita.
-
-    Vou utilizar o loop WHILE para esse movimento.*/
-
-    printf("\nMovimento do Bispo:\n");
-    int casasBispo = 0;
-    while (casasBispo < 5) {
-        printf("Cima Direita\n");
-        casasBispo++;
+// Criando a função recursiiva do Bispo
+void moverBispo(int casas) { //Variável casas indica a quantidade de vezes que a função será chamada.
+    for (int vertical = casas; vertical > 0; vertical--) {
+        printf("Cima "); //Loop externo: Indica a direção vertical e imprime a movimentação do Bispo
+        for (int horizontal = 0; horizontal < 1; horizontal++) {
+            printf("Direita\n"); //Loop interno: Indica a direção horizontal e imprime a movimentação do Bispo
+        }
+        moverBispo(vertical - 1); //Recursividade
+        return;
+        //Esses loops estão programados para sempre repetir apenas 1 vez, sempre que a função for chamada.
     }
+}
 
-    /*RAINHA
-    
-    A rainha pode se mover em qualquer direção, quantas casas forem possíveis. Neste exemplo, a rainha irá 
-    andar 8 casas para a esquerda.
-
-    Vou utilizar o loop DO-WHILE para esse movimento.*/
-
-    printf("\nMovimento da Rainha:\n");
-    int casasRainha = 0;
-    do {
+// Criando a função recursiiva da Rainha
+void moverRainha(int casas) { //Variável casas indica a quantidade de vezes que a função será chamada.
+    if (casas > 0) {
         printf("Esquerda\n");
-        casasRainha++;
-    } while (casasRainha < 8);
+        moverRainha(casas - 1); //Recursividade
+    }
+}
 
-    /*CAVALO
-    O cavalo pode se mover em forma de L, ou seja, 2 casas na vertical ou horizontal e mais 1 casa
-    perpendicular a direção escolhida anteriormente.
-    
-    Vou utilizar o loop FOR para movimentar as 2 casas, para baixo e o loop WHILE,
-    para mover para 1 casa perpendicular para a esquerda
-    Os loops estão aninhados, como pedido*/
-    
-    printf("\nMovimento do Cavalo:\n");
-    int casasCavalo = 2;
-    
+int main() {
+    //Chamando as funçôes recursivas
+    printf("Movimento da Torre\n");
+    moverTorre(5); //5 representa a quantidade de vezes que a peça será movida
+    printf("\n");
+
+    printf("Movimento do Bispo\n");
+    moverBispo(5); //5 representa a quantidade de vezes que a peça será movida
+    printf("\n");
+
+    printf("Movimento da Rainha\n");
+    moverRainha(8); //8 representa a quantidade de vezes que a peça será movida
+    printf("\n");
+
+    //Cavalo sem Recursividade
+    printf("Movimento do Cavalo:\n");
+    int casasCavalo = 2; //Variável casasCavalo indica a quantidade de movimentos que a peça faz, sempre serão 3 movimentos.
     for (int cavalo = 0; cavalo <= casasCavalo; cavalo++) {
-        printf("Baixo\n");
+        printf("Cima\n"); //Loop externo: Responsável pela movimentação da peça, sempre movendo 2 casas.
         casasCavalo--;
           
-        while (cavalo == 1) {
-            printf("Esquerda\n");
-            casasCavalo = casasCavalo + 2;
-            cavalo ++;
+        while (cavalo == 1) { //Loop interno: Responsável pela movimentação da peça, sempre movendo 1 casa perperdicular a movimentação do loop externo.
+            printf("Direita\n");
+            casasCavalo = casasCavalo + 2; //Após os 3 movimentos da peça, a variável casasCavalo voltará a seu valor inicial
+            cavalo ++; //Finalizando o loop, impedindo erros
         }
     }
-
+    //Loops aninhados
     return 0;
 }
